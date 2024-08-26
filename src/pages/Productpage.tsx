3f767a47990db9bfb2bf8cc909cb566f786795fe
reshad21 <miakala7@gmail.com>
2024-08-26 23:52:23 +0600

    successfully add redux setup

--- src/pages/Productpage.tsx
+++ src/pages/Productpage.tsx
@@ -1,7 +1,27 @@
+import { decrement, increment } from "../redux/features/cart/counterSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+
 const Productpage = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  console.log(count);
+  const dispatch = useAppDispatch();
   return (
     <div>
-      <h1>Product will be displayed here</h1>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
