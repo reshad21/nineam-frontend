1298e11db07f9e710fad88d38df7b7f699cad207
reshad21 <miakala7@gmail.com>
2024-08-28 16:46:13 +0600

    handle token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,12 +1,59 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logOut, setUser } from "../features/auth/authSlice";
+import { RootState } from "../store";
+
+
+
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:5000/api',
+    credentials: 'include',
+    prepareHeaders: (headers, { getState }) => {
+        const token = (getState() as RootState).auth.token;
+
+        if (token) {
+            headers.set('authorization', `${token}`)
+        }
+        return headers;
+    }
+})
+
+const baseQueryWithRefreshToken: BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType> = async (args, api, extraOptions): Promise<any> => {
+    let result = await baseQuery(args, api, extraOptions);
+    // console.log(result);
+
+    if (result.error?.status == 401) {
+        //*send refresh token
+        console.log('sending refresh token');
+        const res = await fetch('http://localhost:5000/api/auth/refresh-token', {
+            method: 'POST',
+            credentials: 'include',
+        });
+        const data = await res.json();
+
+        if (data.data.accessToken) {
+
+            const user = (api.getState() as RootState).auth.token;
+
+            api.dispatch(setUser({
+                user,
+                token: data.data.accessToken,
+            }))
+
+            result = await baseQuery(args, api, extraOptions)
+        } else {
+            api.dispatch(logOut())
+        }
+    }
+
+    return result;
+}
+
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     // baseQuery: fetchBaseQuery({ baseUrl: 'https://sporting-goods-db.vercel.app' }),
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000/api',
-        credentials: 'include',
-    }),
+    baseQuery: baseQueryWithRefreshToken,
     tagTypes: ['bikes'],
     endpoints: () => ({}),
 });
